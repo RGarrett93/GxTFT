91c0c21ffa9a5290df1334b75b61365947535e6f
RGarrett93 <60946160+RGarrett93@users.noreply.github.com>
2021-01-28 15:24:47 +0000

    Create Nucleo_144.h

---
+++ src/GxIO/STM32MICRO/Nucleo_144/Nucleo_144.h
@@ -0,0 +1,40 @@
+
+#ifndef _Nucleo_144_H_
+#define _Nucleo_144_H_
+
+#include <GxIO/GxIO.h>
+
+class Nucleo_144 : public GxIO
+{
+  public:
+    Nucleo_144(bool bl_active_high = true);
+    const char* name = "Nucleo_144";
+    void reset();
+    void init();
+    uint8_t readDataTransaction();
+    uint16_t readData16Transaction();
+    uint8_t readData();
+    uint16_t readData16();
+   // uint32_t readRawData32(uint8_t part); // debug purpose
+    void writeCommandTransaction(uint8_t c);
+    void writeCommand16Transaction(uint16_t c);
+    void writeDataTransaction(uint8_t d);
+    void writeData16Transaction(uint16_t d, uint32_t num = 1);
+    void writeCommand(uint8_t c);
+    void writeCommand16(uint16_t c);
+    void writeData(uint8_t d);
+    void writeData(uint8_t* d, uint32_t num);
+    void writeData16(uint16_t d, uint32_t num = 1);
+    void writeAddrMSBfirst(uint16_t d);
+    void startTransaction();
+    void endTransaction();
+    void selectRegister(bool rs_low); // for generalized readData & writeData (RA8875)
+    void setBackLight(bool lit);
+  private:
+    int8_t _cs, _rs, _rst, _wr, _rd, _bl; // Control lines
+    bool _bl_active_high;
+};
+
+#define GxIO_Class Nucleo_144
+
+#endif
